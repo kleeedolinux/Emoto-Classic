e8078def17b8d615c614241f228b6412a2efa172
Pedro Figueiredo <pedroernestofsf@gmail.com>
2023-02-07 13:53:18 -0300

    cleaned

--- public/scripts/dist/autocomplete.d.ts
+++
@@ -1,8 +0,0 @@
-declare class Autocomplete {
-    private input;
-    private suggestions;
-    constructor(inputId: string, suggestions: string[]);
-    private init;
-    private handleInput;
-    private renderSuggestions;
-}
--- public/scripts/dist/emote.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=emote.js.map
